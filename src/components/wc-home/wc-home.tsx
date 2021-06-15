f5e38825682ba454d59e91baaf5ea07df38362ff
git-anurag-hub <aguptaking@gmail.com>
2021-06-15 23:53:04 +0530

    Copy Text

--- src/components/wc-home/wc-home.tsx
+++ src/components/wc-home/wc-home.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, State } from '@stencil/core';
 
 @Component({
   tag: 'wc-home',
@@ -11,7 +11,7 @@ export class WcHome {
       <Host>
         <div class="relative">
           <div class="absolute right-10 md:right-40 -top-28">
-            <img src="https://raw.githubusercontent.com/elixir-cloud-aai/logo/dev/logo.svg" class="inline-block mx-3 opacity-20 w-108"></img>
+            <img src="https://raw.githubusercontent.com/elixir-cloud-aai/logo/dev/logo.svg" class="inline-block mx-3 opacity-20 w-96 md:w-108"></img>
           </div>
           <div class="mx-10 md:ml-40 mt-28 mb-40 md:mb-56 md:mr-108 md:mt-40 static">
             <div class="text-4xl font-bold md:text-5xl text-gray-700">
@@ -25,7 +25,22 @@ export class WcHome {
               Get Started
             </button>
             <button class="text-lg ml-0 mt-5 px-5 bg-gray-200 p-2 focus:outline-none shadow-inner rounded-xl w-full md:w-max md:ml-10 md:mt-0">
-              $ npm i @elixir/web-components
+              <span class="mr-2 text-gray-400">$</span> npm i @elixir/web-components
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 inline ml-4 text-gray-400 hover:text-black"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                onClick={() => navigator.clipboard.writeText('npm i @elixir/web-components')}
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                />
+              </svg>
             </button>
           </div>
         </div>
