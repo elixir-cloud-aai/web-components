2181966d6ed3806fc395bc4aac6aa8de41ab2d32
git-anurag-hub <aguptaking@gmail.com>
2021-08-04 16:55:37 +0530

    Tests

--- src/components/wc-elixir-utils-new-service/test/wc-elixir-utils-new-service.spec.tsx
+++ src/components/wc-elixir-utils-new-service/test/wc-elixir-utils-new-service.spec.tsx
@@ -1,6 +1,53 @@
-// import { newSpecPage } from '@stencil/core/testing';
-// import { WcElixirUtilsNewService } from '../wc-elixir-utils-new-service';
+import { newSpecPage } from '@stencil/core/testing';
+import { WcElixirUtilsNewService } from '../wc-elixir-utils-new-service';
 
-describe('wc-elixir-utils-new-service', () => {
-  it('renders', async () => {});
+describe('wc-elixir-utils-manage-permissions', () => {
+  it('does builds', () => {
+    expect(new WcElixirUtilsNewService()).toBeTruthy();
+  });
+
+  describe('has props', () => {
+    it('has authToken prop', async () => {
+      const page = await newSpecPage({
+        components: [WcElixirUtilsNewService],
+        html: '<div></div>',
+      });
+
+      let component = page.doc.createElement('wc-elixir-utils-new-service');
+      component.authToken = 'component-demo';
+      page.root.appendChild(component);
+      await page.waitForChanges();
+
+      expect(page.rootInstance.authToken).toBe('component-demo');
+    });
+
+    // it('has itemsPerPage prop', async () => {
+    //   const page = await newSpecPage({
+    //     components: [WcElixirUtilsNewService],
+    //     html: '<div></div>',
+    //   });
+
+    //   let component = page.doc.createElement('wc-elixir-utils-new-service');
+    //   component.authToken = 'component-demo';
+    //   component.itemsPerPage = 10;
+    //   page.root.appendChild(component);
+    //   await page.waitForChanges();
+
+    //   expect(page.rootInstance.itemsPerPage).toBe(10);
+    // });
+  });
+
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [WcElixirUtilsNewService],
+      html: '<div></div>',
+    });
+
+    let component = page.doc.createElement('wc-elixir-utils-new-service');
+    component.authToken = 'component-demo';
+    page.root.appendChild(component);
+    await page.waitForChanges();
+
+    expect(page).toMatchSnapshot();
+  });
 });
