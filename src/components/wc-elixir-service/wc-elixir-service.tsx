b38dc8e73f281148664d2905c19056d6405275e6
git-anurag-hub <aguptaking@gmail.com>
2021-08-15 16:28:14 +0530

    Service Component

--- src/components/wc-elixir-service/wc-elixir-service.tsx
+++ src/components/wc-elixir-service/wc-elixir-service.tsx
@@ -25,9 +25,18 @@ export class WcElixirService {
     if (this.showManagePermission) {
       return (
         <div>
-          <div class="text-center">
-            <div class="text-lg font-semibold">Manage Permissions</div>
-            <div class="text-gray-700">{this.serviceName}</div>
+          <div class="flex items-center">
+            <div class="text-sm flex items-center cursor-pointer" onClick={() => (this.showManagePermission = false)}>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+              </svg>
+            </div>
+            <div class="flex-grow text-center">
+              <div class="text-center">
+                <div class="text-lg font-semibold">Manage Permissions</div>
+                <div class="text-gray-700">{this.serviceName}</div>
+              </div>
+            </div>
           </div>
           <br></br>
           <wc-elixir-utils-manage-permissions authToken="component-demo"></wc-elixir-utils-manage-permissions>
@@ -37,8 +46,13 @@ export class WcElixirService {
     if (this.showAddService) {
       return (
         <div>
-          <div class="text-center">
-            <div class="text-lg font-semibold">Create new serivce</div>
+          <div class="flex items-center">
+            <div class="text-sm flex items-center cursor-pointer" onClick={() => (this.showAddService = false)}>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+              </svg>
+            </div>
+            <div class="text-lg font-semibold flex-grow text-center">Create new serivce</div>
           </div>
           <br></br>
           <wc-elixir-utils-new-service apiUrl="https://trs-filer-test.rahtiapp.fi/ga4gh/trs/v2/openapi.json" schema="ServiceRegister"></wc-elixir-utils-new-service>
